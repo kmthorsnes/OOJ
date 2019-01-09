2977d9bd3b1ff5d24afd8dffe3703fe73b1d1929
kalle <kmthorsnes@gmail.com>
2019-01-09 08:04:33 +0100

    last part of creating classes

--- js/Game.js
+++ js/Game.js
@@ -1,1 +1,20 @@
-class Board {}
+class Board {
+constructor(){
+    board = new Board();
+    players = this.createPlayers(),
+    ready = false;
+
+}
+/** 
+ * Creates two player objects
+ * @return  {Array}    An array of two Player objects.
+ */
+
+createPlayers(){
+    const players = [new Player('Player 1', 1, '#e15258', true),
+    new Player('Player 2', 2, '#e59a13'];
+    return players 
+
+}
+
+}
